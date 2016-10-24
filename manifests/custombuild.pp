5d3b96596a22bc7cc631ca0a24304d3189b03468
sensson <accounts@sensson.net>
2016-10-24 12:25:36 +0200

    Add the custombuild package to directadmin::params

--- manifests/custombuild.pp
+++ manifests/custombuild.pp
@@ -1,11 +1,9 @@
 # directadmin::custombuild
-class directadmin::custombuild {
-  $custombuild_installer = 'http://files.directadmin.com/services/custombuild/2.0/custombuild.tar.gz'
-
+class directadmin::custombuild inherits directadmin {
   # Exec: download the latest custombuild version
   exec { 'install-custombuild':
     cwd     => '/usr/local/directadmin',
-    command => "rm -rf custombuild* && wget --no-check-certificate -O custombuild.tar.gz ${custombuild_installer} && tar xvzf custombuild.tar.gz",
+    command => "rm -rf custombuild* && wget --no-check-certificate -O custombuild.tar.gz ${::directadmin::params::custombuild_installer_location} && tar xvzf custombuild.tar.gz",
     creates => '/usr/local/directadmin/custombuild/options.conf',
     require => File['/usr/local/directadmin'],
     path    => '/bin:/usr/bin',
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,8 @@
 # params
 class directadmin::params {
   # The installation URL and a set of base packages that we need to install
-  $installer_location     = 'http://www.directadmin.com/setup.sh'
+  $installer_location             = 'http://www.directadmin.com/setup.sh'
+  $custombuild_installer_location = 'http://files.directadmin.com/services/custombuild/2.0/custombuild.tar.gz'
 
   # Mail settings
   $default_webmail        = 'roundcube'
