7d14fa65b17561a836368594527b2e6cb2cacf78
ju5t <m@ju5t.nl>
2017-02-16 19:14:11 +0100

    Add support for managing /etc/virtual files

---
+++ manifests/mail/exim/virtual.pp
@@ -0,0 +1,22 @@
+# directadmin::mail::exim::skip_rbl
+define directadmin::mail::exim::virtual(
+  $value = undef,
+  $file  = undef,
+) {
+  # Check the $file parameter
+  if $file == undef { fail('file must be set.') }
+  if $file =~ /\/etc\/virtual/ { fail('file should be specified without /etc/virtual/') }
+
+  # Check $value parameter - we don't do consistency checks!
+  if $value == undef { fail('value must be set.') }
+
+  # Manage /etc/virtual/${file}
+  ensure_resource('file', "/etc/virtual/${file}", { 'ensure' => 'present', 'mode' => '0644'} )
+
+  file_line { "exim-set-${file}-${value}":
+    path    => "/etc/virtual/${file}",
+    line    => $value,
+    require => File["/etc/virtual/${file}"],
+    notify  => Service['exim'],
+  }
+}
---
+++ spec/defines/directadmin_mail_exim_virtual_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+describe 'directadmin::mail::exim::virtual', :type => :define do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        let(:title) do
+          'foobar'
+        end
+
+        let(:pre_condition) do
+          'class { "::directadmin": clientid => 1234, licenseid => 123456 }'
+        end
+
+        context "directadmin::mail::exim::virtual define without parameters" do
+          it 'fails' do
+            expect { subject.call } .to raise_error(/file must be set/)
+          end
+        end
+
+        context "directadmin::mail::exim::virtual define with /etc in a file" do
+          let (:params) {{
+            :file => '/etc/virtual/skip_rbl_hosts_ip'
+          }}
+
+          it 'fails' do
+            expect { subject.call } .to raise_error(/file should be specified without/)
+          end
+        end
+
+        context "directadmin::mail::exim::virtual define without a value" do
+          let (:params) {{
+            :file => 'skip_rbl_hosts_ip'
+          }}
+
+          it 'fails' do
+            expect { subject.call } .to raise_error(/value must be set/)
+          end
+        end
+
+        context "directadmin::mail::exim::virtual define with parameters" do
+          let(:params) {{
+            :file => 'skip_rbl_hosts_ip',
+            :value => '127.0.0.1'
+          }}
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_file('/etc/virtual/skip_rbl_hosts_ip') }
+          it { is_expected.to contain_file_line('exim-set-skip_rbl_hosts_ip-127.0.0.1') }
+          it { is_expected.to contain_file_line('exim-set-skip_rbl_hosts_ip-127.0.0.1').with_path('/etc/virtual/skip_rbl_hosts_ip') }
+          it { is_expected.to contain_file_line('exim-set-skip_rbl_hosts_ip-127.0.0.1').with_line('127.0.0.1') }
+          it { is_expected.to contain_file_line('exim-set-skip_rbl_hosts_ip-127.0.0.1').that_requires('File[/etc/virtual/skip_rbl_hosts_ip]') }
+          it { is_expected.to contain_file_line('exim-set-skip_rbl_hosts_ip-127.0.0.1').that_notifies('Service[exim]') }
+        end
+
+      end
+    end 
+  end
+end
