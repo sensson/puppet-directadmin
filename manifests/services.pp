9cb382bc14f5fa422b2e86d6e58d5a2a6e78fc1b
Pascal Osinga <posinga@anwb.nl>
2020-03-06 12:09:07 +0100

    Enable/Disable services

--- manifests/services.pp
+++ manifests/services.pp
@@ -1,27 +1,35 @@
 # directadmin::services
 class directadmin::services {
+  $exim = lookup({ 'name' => 'directadmin::services:exim', 'default_value' => true })
+  $dovecot = lookup({ 'name' => 'directadmin::services:dovecot', 'default_value' => true })
+  $named = lookup({ 'name' => 'directadmin::services:named', 'default_value' => true })
+
   case $::operatingsystem {
     'RedHat', 'CentOS':   { $has_status = true }
     /^(Debian|Ubuntu)$/:  { $has_status = false }
     default:              { $has_status = true }
   }
 
-  # Service: exim, our e-mail server
-  service { 'exim':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => $has_status,
-    require    => Exec['directadmin-installer'],
+  if $exim {
+    # Service: exim, our e-mail server
+    service { 'exim':
+      ensure     => running,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => $has_status,
+      require    => Exec['directadmin-installer'],
+    }
   }
 
-  # Service: dovecot, our POP/IMAP server
-  service { 'dovecot':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => $has_status,
-    require    => Exec['directadmin-installer'],
+  if $dovecot {
+    # Service: dovecot, our POP/IMAP server
+    service { 'dovecot':
+      ensure     => running,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => $has_status,
+      require    => Exec['directadmin-installer'],
+    }
   }
 
   # Service: apache, our web server
@@ -42,13 +50,15 @@ class directadmin::services {
     require    => Exec['directadmin-installer'],
   }
 
-  # Service: named, our dns server
-  service { 'named':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => $has_status,
-    require    => Exec['directadmin-installer'],
+  if $dovecot {
+    # Service: named, our dns server
+    service { 'named':
+      ensure     => running,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => $has_status,
+      require    => Exec['directadmin-installer'],
+    }
   }
 
   # Service: mysqld, our database server
